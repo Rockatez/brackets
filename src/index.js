362f4be572ab436cc32a226f518fa008681aa383
Artem Rockatez <razvorotkapr@gmail.com>
2022-08-05 15:46:34 +0200

    add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const clearBrackets = bracketsConfig.map(item => item.join(''));
+  for (let i = 0; i < clearBrackets.length; i++) {
+              if(str.includes(clearBrackets[i])) {
+              str = str.replace(clearBrackets[i], '')
+              i = -1
+              }
+  }
+  if (str.length % 2 !== 0 ) {
+    return false;
+  }
+    return str = !str
+  }
